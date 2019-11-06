cf21b72e5f9c8412d66e093643c0742c9570959b
hrozan <higorb.rozan@gmail.com>
2019-11-06 13:12:45 -0300

    :construction_worker: add ci to front

---
+++ deploy/client.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FILE_NAME="client.tar.gz"
+HOST="hrozan@hrozan.xyz"
+
+echo "🏁 Start Client Deploy"
+cd fp-client/
+
+echo "⚙️ Building"
+npm run build --env production
+
+echo "📦 Packing"
+mv build client
+tar -czvf $FILE_NAME ./client
+
+echo "🚀 Publish" 
+scp $FILE_NAME $HOST:/home/hrozan/
+ssh $HOST "tar -xvzf ${FILE_NAME}"
+
+echo "🗑 Cleaning"
+rm $FILE_NAME
+rm -rf client
+ssh $HOST "rm ${FILE_NAME}"
