f787b948f27d8cfba764e2e0dd9fbcde5db5e456
hrozan <higorb.rozan@gmail.com>
2019-11-06 13:38:57 -0300

    :fire: remove pub.sh

--- fp-rasp/pub.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mosquitto_pub -h hrozan.xyz -t test -m "hello again" -p 8883 --capath /etc/ssl/certs/ -u "hrozan" -P "t1i2i5"
-#mosquitto_sub -h hrozan.xyz -p 8883 --capath /etc/ssl/certs/  -t test -u "hrozan" -P "t1i2i5"
