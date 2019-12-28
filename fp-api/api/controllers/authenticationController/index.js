c55fd1472be5b9961363b1113b1548752f40012c
Higor Rozan <higorb.rozan@gmail.com>
2019-12-27 23:00:47 -0300

    :white_check_mark: test(login) add login test

--- fp-api/api/controllers/authenticationController/index.js
+++ fp-api/api/controllers/authenticationController/index.js
@@ -4,34 +4,30 @@ const { JWT_KEY } = process.env
 
 const LOGIN_ERROR_RESPONSE = { message: "Ops, your credential are wrong, try again." }
 
-const authController = {
-	async login(req, res) {
-		const { username, password } = req.body
-		console.log(`Login attempt: ${username} `)
-		try {
-			const user = await User.findOne({ username })
-			if (!user) {
-				console.log("User not Found")
-				return res.status(400).json(LOGIN_ERROR_RESPONSE)
-			}
-
-			const match = await user.checkPassword(password)
-			if (!match) {
-				console.log("Wrong Password")
-				return res.status(400).json(LOGIN_ERROR_RESPONSE)
-			}
-
-			const payload = {
-				id: user._id,
-				username: user.username
-			}
-			const token = jwt.sign(payload, JWT_KEY)
-
-			res.status(200).json({ token, ...payload })
-		} catch (error) {
-			res.status(500)
+exports.login = async (request, response) => {
+	const { username, password } = request.body
+	console.log(`Login attempt: ${username} `)
+	try {
+		const user = await User.findOne({ username })
+		if (!user) {
+			console.log("User not Found")
+			return response.status(400).json(LOGIN_ERROR_RESPONSE)
 		}
+
+		const match = await user.checkPassword(password)
+		if (!match) {
+			console.log("Wrong Password")
+			return response.status(400).json(LOGIN_ERROR_RESPONSE)
+		}
+
+		const payload = {
+			id: user._id,
+			username: user.username
+		}
+		const token = jwt.sign(payload, JWT_KEY)
+
+		return response.status(200).json({ token, ...payload })
+	} catch (error) {
+		response.status(500)
 	}
 }
-
-module.exports = authController
--- fp-api/api/routes.js
+++ fp-api/api/routes.js
@@ -1,24 +1,24 @@
 const express = require("express")
 const api = express.Router()
-const auth = require("./controllers/authenticationController")
+const authenticationController = require("./controllers/authenticationController")
 const smartObject = require("./controllers/smartObjectController")
 const userController = require("./controllers/userController")
 const authentication = require("./middleware/authentication")
 
-/* GET home page. */
 api.route("/").get((req, res) => res.json({ status: "ok" }))
-api.route("/login").post(auth.login)
-api.route("/smart-object").get(authentication.middleware, smartObject.read)
+api.route("/login").post(authenticationController.login)
 api.route("/mqtt/credentials").get(authentication.middleware, smartObject.getCredentials)
 
 // User
 api
 	.route("/user")
+	.all(authentication.middleware)
 	.get(userController.list)
 	.post(userController.create)
 
 api
 	.route("/user/:id")
+	.all(authentication.middleware)
 	.get(userController.read)
 	.delete(userController.delete)
 
