988daa0f8a477917efe2eca9cc51c6f2a3db1f34
Higor Rozan <higor.rozan@lambda3.com.br>
2020-08-01 13:11:06 -0300

    :white_check_mark: test(smart-object): Add app test

--- smart-object/tests/test_app.py
+++ smart-object/tests/test_app.py
@@ -1,18 +1,23 @@
+from unittest.mock import Mock
+
 from app.config import ApiConfig
 from app.messenger import create_messenger_client, MessengerConfig, BrokerCredentials
 from app.webapi import create_api_service
-from tests.mock import MockResponse
 
 
 def test_create_api_service_successfully(mocker):
     token = "test-token"
-    mock_response = MockResponse(body={'token': token})
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'token': token}
+
     mocker.patch('requests.post', return_value=mock_response)
     api_config = ApiConfig("http://localhost:3000", "higor@email.com", "pass123")
 
     api_service = create_api_service(api_config)
 
     assert api_service.token == token
+    assert mock_response.json.called
 
 
 def test_create_messenger_client(mocker):
@@ -24,4 +29,7 @@ def test_create_messenger_client(mocker):
     client = create_messenger_client(messenger_config, credentials)
 
     assert mocked_client.return_value == client
-    # assert mocked_client.mock_calls[1] == call.tls_set()
+    assert client.tls_set.called
+    assert client.username_pw_set.called
+    assert client.connect.called
+
